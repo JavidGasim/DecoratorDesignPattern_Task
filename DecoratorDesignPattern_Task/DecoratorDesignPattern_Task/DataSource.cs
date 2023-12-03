aac731714622e34ab3c2421821239d0867965191
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 16:39:42 +0400

    Add files via upload

---
+++ DecoratorDesignPattern_Task/DecoratorDesignPattern_Task/DataSource.cs
@@ -0,0 +1,83 @@
+﻿namespace DecoratorDesignPattern_Task;
+
+public interface DataSource
+{
+    public void writeData(string data);
+    public void readData();
+}
+
+public class FileDataSource : DataSource
+{
+    private string fileName;
+
+    public FileDataSource(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public void readData()
+    {
+        Console.WriteLine("Read Data");
+    }
+
+    public void writeData(string data)
+    {
+        Console.WriteLine("Write Data");
+    }
+}
+
+public class DataSourceDecorator : DataSource
+{
+    private DataSource wrappee;
+
+    public DataSourceDecorator(DataSource wrappee)
+    {
+        this.wrappee = wrappee;
+    }
+
+    public void readData()
+    {
+        Console.WriteLine("Read Data");
+    }
+
+    public void writeData(string data)
+    {
+        Console.WriteLine("Write Data");
+    }
+}
+
+public class EncyrptionDecorator : DataSourceDecorator
+{
+    public EncyrptionDecorator(DataSource wrappee) : base(wrappee)
+    {
+
+    }
+
+    public void readData()
+    {
+        Console.WriteLine("Read Data");
+    }
+
+    public void writeData(string data)
+    {
+        Console.WriteLine("Write Data");
+    }
+}
+
+public class CompressionDecorator : DataSourceDecorator
+{
+    public CompressionDecorator(DataSource wrappee) : base(wrappee)
+    {
+
+    }
+
+    public void readData()
+    {
+        Console.WriteLine("Read Data");
+    }
+
+    public void writeData(string data)
+    {
+        Console.WriteLine("Write Data");
+    }
+}
